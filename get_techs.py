6ca7e72663d049e3a44db22944669a69cbf43e41
AlinCqe <alinemanuelv@gmail.com>
2026-08-26 23:29:39 +0300

    feat: detect wdpress plugins and themes

--- get_techs.py
+++ get_techs.py
@@ -1,12 +1,29 @@
 import os
 import re
 import json
-
+from urllib.parse import urlparse, urlsplit, urlunsplit
 
 def load_apps(filename="apps.json"):
 
     filename = os.path.join(os.getcwd(), os.path.dirname(__file__), filename)
     return json.load(open(filename))
+
+def extract_wordpress_component(resource_url, component_type):
+    path = urlparse(resource_url).path
+    parts = [part for part in path.split("/") if part]
+
+    sequence = ["wp-content", component_type]
+
+    sequence_length = len(sequence)
+
+    for index in range(len(parts) - sequence_length):
+        current_sequence = parts[index:index + sequence_length]
+
+        if current_sequence == sequence:
+            component_index = index + sequence_length
+            return parts[component_index]
+        
+    return None
 
 
 data = load_apps()
@@ -21,9 +38,11 @@ def get_techs(page_data: dict) -> dict :
 
     url = page_data.get("final_url")
     response_headers = page_data.get("main_response_headers")
-
     raw_html = page_data.get("raw_html")
     rendered_html = page_data.get("rendered_html")
+    page_scripts = page_data["scripts"];
+    page_links = page_data["links"];
+    network_requets = page_data.get("network_requests", [])
 
     # URL tech detection
     if url:
@@ -107,7 +126,7 @@ def get_techs(page_data: dict) -> dict :
                             page_result["technologies"][app_name] = {
                                 "proof": {
                                     "source": "html",
-                                    "matched": match.group(0),
+                                    "matched": match.group(0)[:300]
                                 }
                             }
     # meta tags detection in same html loop
@@ -133,15 +152,13 @@ def get_techs(page_data: dict) -> dict :
                             page_result["technologies"][app_name] = {
                                 "proof": {
                                     "source": "meta tags",
-                                    "matched": match.group(0),
+                                    "matched": match.group(0)[:300]
                                 }
                             }
                         break
 
 
     # #requests tech detection 
-    network_requets = page_data.get("network_requests", [])
-
     for request in network_requets:
         request_url = request.get("url")
         resource_type = request.get("resource_type")
@@ -174,5 +191,76 @@ def get_techs(page_data: dict) -> dict :
                                     }
                                 }  
                             break
-                    
+
+
+    # saves all links from html and network requests
+    resource_urls = []
+    
+    for script in page_data.get("scripts", []):
+        script_url = script.get("src")
+
+        if script_url:
+            resource_urls.append({
+                "url": script_url,
+                "source": "script tag"
+            })
+
+    for link in page_data.get("links", []):
+        link_url = link.get("href")
+
+        if link_url:
+            resource_urls.append({
+                "url": link_url,
+                "source": "link tag"
+            })
+
+    for request in page_data.get("network_requests", []):
+        request_url = request.get("url")
+
+        if request_url:
+            resource_urls.append({
+                "url": request_url,
+                "source": "network request"
+            })
+            
+    # checks for wdpress plugins or themes 
+        
+    for resource in resource_urls:
+        resource_link = resource.get("url")
+        resource_source = resource.get("source")
+        if not resource_link:
+            continue
+
+        plugin_name = extract_wordpress_component(
+            resource_link,
+            "plugins"
+        )
+
+        if plugin_name:
+            technology_name = f"WordPress Plugin: {plugin_name}"
+
+            if technology_name not in page_result["technologies"]:
+                page_result["technologies"][technology_name] = {
+                    "proof": {
+                        "source": resource_source,
+                        "url": resource_link
+                    }
+                }
+
+        theme_name = extract_wordpress_component(
+            resource_link,
+            "themes"
+        )
+
+        if theme_name:
+            technology_name = f"WordPress Theme: {theme_name}"
+
+            if technology_name not in page_result["technologies"]:
+                page_result["technologies"][technology_name] = {
+                    "proof": {
+                        "source": resource_source,
+                        "url": resource_link
+                    }
+                }
+
     return page_result
